3da5932965562dd2d2368802799c61eb7ad22e38
SeungMiL <tmskqj@naver.com>
2022-04-20 23:21:43 +0900

    1

--- js/main.js
+++ js/main.js
@@ -30,3 +30,8 @@ $(".alert_ex").on("click", function () {
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
+window.addEventListener("resize", () => {
+  console.log("resize");
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
