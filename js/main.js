df2ab2392fa8cf530e2c12b67a8b30751576f535
SeungMiL <tmskqj@naver.com>
2022-04-20 23:18:04 +0900

    1

--- js/main.js
+++ js/main.js
@@ -27,10 +27,6 @@ $(".alert_ex").on("click", function () {
 });
 
 
-// let vh = window.innerHeight * 0.01;
-// document.documentElement.style.setProperty('--vh', `${vh}px`);
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-// window.addEventListener('resize', function(){
-//   let vh = window.innerHeight * 0.01;
-//   document.documentElement.style.setProperty('--vh', `${vh}px`)
-// })
