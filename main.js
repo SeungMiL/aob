c10a9d79eae8f605cbbd69d4677e0518dcbaee7c
SeungMiL <tmskqj@naver.com>
2022-04-20 15:05:23 +0900

    4

---
+++ main.js
@@ -0,0 +1,19 @@
+$(".page_ex").on("click", function () {
+  $(".container1").toggle("on");
+});
+
+
+$(".layer_ex").on("click", function () {
+    $(".container3").toggle("on");
+  });
+
+  
+  $(".popup_ex").on("click", function () {
+    $(".container2").toggle("on");
+  });
+
+  
+  $(".alert_ex").on("click", function () {
+    $(".container4").toggle("on");
+  });
+  
